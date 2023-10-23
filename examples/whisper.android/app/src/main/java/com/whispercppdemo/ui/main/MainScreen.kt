2f5653c413700f6b3fe5e734dd8c68aa7e121ddf
Hunter Sun <HunterSun2018@gmail.com>
2023-10-23 17:51:55 +0800

    enabled text to scroll to bottom after transcribing data

--- examples/whisper.android/app/src/main/java/com/whispercppdemo/ui/main/MainScreen.kt
+++ examples/whisper.android/app/src/main/java/com/whispercppdemo/ui/main/MainScreen.kt
@@ -1,11 +1,15 @@
 package com.whispercppdemo.ui.main
 
+import android.text.method.ScrollingMovementMethod
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -42,32 +46,48 @@ private fun MainScreen(
                 title = { Text(stringResource(R.string.app_name)) }
             )
         },
+        floatingActionButton = {
+            RecordButton(
+                enabled = canTranscribe,
+                isRecording = isRecording,
+                onClick = onRecordTapped
+            )
+        }
     ) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .padding(16.dp)
+                .padding(start=16.dp, top=16.dp, end=16.dp, bottom = 100.dp)
         ) {
-            Column(verticalArrangement = Arrangement.SpaceBetween) {
-                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
-                    BenchmarkButton(enabled = canTranscribe, onClick = onBenchmarkTapped)
-                    TranscribeSampleButton(enabled = canTranscribe, onClick = onTranscribeSampleTapped)
-                }
-                RecordButton(
-                    enabled = canTranscribe,
-                    isRecording = isRecording,
-                    onClick = onRecordTapped
-                )
-            }
+
             MessageLog(messageLog)
+
+//            Column(verticalArrangement = Arrangement.SpaceBetween) {
+//                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+//                    BenchmarkButton(enabled = canTranscribe, onClick = onBenchmarkTapped)
+//                    TranscribeSampleButton(enabled = canTranscribe, onClick = onTranscribeSampleTapped)
+//                }
+//                RecordButton(
+//                    enabled = canTranscribe,
+//                    isRecording = isRecording,
+//                    onClick = onRecordTapped
+//                )
+//            }
+
         }
     }
 }
 
 @Composable
 private fun MessageLog(log: String) {
-    SelectionContainer {
-        Text(modifier = Modifier.verticalScroll(rememberScrollState()), text = log)
+    SelectionContainer (
+    ){
+        var logState = rememberScrollState(0)
+        LaunchedEffect(log) {
+            logState.scrollTo(logState.maxValue)
+        }
+
+        Text(modifier = Modifier.verticalScroll(logState), text = log)
     }
 }
 
